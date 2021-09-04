0a5deb8cac84ff5bc031fb810987416dbf5464b5
sudhnsh <sudhnsh16@gmail.com>
2021-09-05 02:29:32 +0530

    codeforces

--- Codeforces/B_Scenes_From_a_Memory.cpp
+++ Codeforces/B_Scenes_From_a_Memory.cpp
@@ -18,22 +18,39 @@ void solve()
             cout<<s[i]<<'\n';
             return;
         }
+        mp[s[i]]++;
+    }
+    rep(i,0,n){
         if((s[i]=='2'||s[i]=='5')&&i!=0)
         {
-            string s1=s.substr(0,(i+1));
-            cout<<i+1<<'\n';
+            string s1=s.substr(i-1,(2));
+            cout<<2<<'\n';
             cout<<s1<<'\n';
             return;
         }
-        mp[s[i]]++;
-        if(mp[s[i]]==2)
+    }
+    rep(i,0,n){
+        if(mp[s[i]]>=2)
         {
             cout<<2<<'\n';
             cout<<s[i]<<s[i]<<'\n';
             return;
         }
-
+        if(mp['2']>=1&&mp['7']>=1)
+        {
+            cout<<2<<'\n';
+            cout<<27<<'\n';
+            return;
+        }
+        if(mp['5']>=1&&mp['7']>=1)
+        {
+            cout<<2<<'\n';
+            cout<<57<<'\n';
+            return;
+        }
+     
     }
+    
 }
 int main()
 {
