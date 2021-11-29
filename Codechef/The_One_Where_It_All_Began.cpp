21cdc08c3d44ee204e7fe2c26b71d1e4a8f489b4
sudhnsh <sudhnsh16@gmail.com>
2021-11-29 22:52:03 +0530

    C.O.D.E.R.S

---
+++ Codechef/The_One_Where_It_All_Began.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define rep(i, a, n) for (ll i = a; i < n; i++)
+#define ffst                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define m 1000000007
+// by sudhnsh
+void solve()
+{
+    ll n;
+    cin>>n;
+    if(n>=21)
+    {
+        cout<<"YES\n";
+    }
+    else
+    {
+        cout<<"NO\n";
+    }
+    
+}
+int main()
+{
+    //ffst;
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
