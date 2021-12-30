a2635bd37b49b1ed7a83ff2758f6575c20a777ab
sudhnsh <sudhnsh16@gmail.com>
2021-12-30 17:14:42 +0530

    Good Bye 2021

--- Codeforces/C_Representative_Edges.cpp
+++ Codeforces/C_Representative_Edges.cpp
@@ -14,36 +14,35 @@ void solve()
     ll n;
     cin >> n;
     vector<double> v(n);
-    rep(i,0,n)
+    rep(i, 0, n)
     {
-        cin>>v[i];
+        cin >> v[i];
     }
-    if(n==1||n==2)
+    if (n == 1 || n == 2)
     {
-       cout<<0<<'\n';
+        cout << 0 << '\n';
     }
     else
     {
-        ll mn=n-1;
-        rep(i,0,n)
+        ll mn = n - 1;
+        rep(i, 0, n)
         {
-            map<double,ll> mp;
-            rep(j,0,n)
+            map<double, ll> mp;
+            rep(j, 0, n)
             {
-                if(i!=j)
-                mp[((v[j]-v[i])/(j-i))]++;
+                if (i != j)
+                    mp[((v[j] - v[i]) / (j - i))]++;
             }
-            ll mx=0;
-            for(auto x:mp)
+            ll mx = 0;
+            for (auto x : mp)
             {
-                mx=max(mx,x.second+1);
+                mx = max(mx, x.second + 1);
             }
-            mn=min(mn,n-mx);
+            mn = min(mn, n - mx);
         }
-        cout<<mn;
-        cout<<'\n';
+        cout << mn;
+        cout << '\n';
     }
-   
 }
 int main()
 {
